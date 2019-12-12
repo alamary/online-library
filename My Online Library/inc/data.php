566b4265afdfabb0bf02ab5156b49eb3171aaf30
alamary <48289661+alamary@users.noreply.github.com>
2019-12-11 19:10:44 -0500

    Add files via upload

---
+++ My Online Library/inc/data.php
@@ -0,0 +1,204 @@
+<?php
+$catalog = [];
+//Books
+$catalog[101] = [
+	"title" => "The Old Man and the Sea",
+	"img" => "img/media/theoldman.jpg",
+    "genre" => "Literary Fiction",
+    "format" => "Paperback",
+    "year" => 1952,
+    "category" => "Books",
+    "authors" => [
+        "Ernest Hemingway"
+    ],
+    "publisher" => "Scribner, Reissue edition (May 5, 1995)",
+    "isbn" => '978-0684801223'
+];
+$catalog[102] = [
+    "title" => "The Sailor Who Fell from Grace with the Sea",
+    "img" => "img/media/thesailorwhofell.jpg",
+    "genre" => "Philosophical Fiction",
+    "format" => "Paperback",
+    "year" => 1963,
+    "category" => "Books",
+    "authors" => [
+        "Yukio Mushima"
+    ],
+    "publisher" => "Vintage (May 31, 1994)",
+    "isbn" => '978-0679750154'
+];
+$catalog[103] = [
+    "title" => "Fahrenheit 451",
+    "img" => "img/media/fahrenheit451.jpg",
+    "genre" => "Dystopian",
+    "format" => "Paperback",
+    "year" => 1953,
+    "category" => "Books",
+    "authors" => [
+        "Ray Bradbury"
+    ],
+    "publisher" => "Simon & Schuster, Reissue edition (January 10, 2012)",
+    "isbn" => '978-1451673319'
+];
+$catalog[104] = [
+    "title" => "Man's Search for Meaning",
+    "img" => "img/media/mansearchformeaning.jpg",
+    "genre" => "Biography",
+    "format" => "Paperback",
+    "year" => 1946,
+    "category" => "Books",
+    "authors" => [
+        "Viktor E. Frankl"
+    ],
+    "publisher" => "Beacon Press, 1 edition (June 1, 2006)",
+    "isbn" => '978-0807014271'
+];
+//Movies
+$catalog[201] = [
+    "title" => "The Avengers",
+    "img" => "img/media/avengers.jpg",
+    "genre" => "Fantasy/Sci-fi",
+    "format" => "DVD/Streaming",
+    "year" => 2012,
+    "category" => "Movies",
+    "director" => "Joss Whedon",
+    "writers" => [
+        "Joss Whedon"
+    ],
+    "stars" => [
+        "Chris Evans",
+        "Robert Downey Jr",
+        "Chris Hemsworth",
+				"Mark Ruffalo",
+				"Scarlett Johansson",
+				"Jeremy Renner",
+				"Tom Hiddleston",
+				"Samuel L. Jackson",
+
+    ]
+];
+$catalog[202] = [
+    "title" => "Avengers: Age of Ultron",
+    "img" => "img/media/ultron.jpg",
+    "genre" => "Sci-fi/Action",
+    "format" => "DVD/Streaming",
+    "year" => 2015,
+    "category" => "Movies",
+    "director" => "Joss Whedon",
+    "writers" => [
+        "Joss Whedon"
+    ],
+    "stars" => [
+			"Robert Downey Jr.",
+			"Chris Evans",
+			"Chris Hemsworth",
+			"Mark Ruffalo",
+			"Scarlett Johansson",
+			"Jeremy Renner",
+			"Don Cheadle",
+			"Aaron Taylor-Johnson",
+			"Idris Elba",
+			"Samuel L. Jackson",
+    ]
+];
+$catalog[203] = [
+    "title" => "Avengers: Infinity War",
+    "img" => "img/media/infinity.jpg",
+    "genre" => "Fantasy/Sci-fi",
+    "format" => "DVD/Streaming",
+    "year" => 2018,
+    "category" => "Movies",
+    "director" => "Anthony Russo",
+									"Joe Russo",
+    "writers" => [
+        "Christopher Markus",
+        "Stephen McFeely"
+    ],
+    "stars" => [
+			"Chris Evans",
+			"Robert Downey Jr.",
+			"Chris Hemsworth",
+			"Mark Ruffalo",
+			"Scarlett Johansson",
+			"Benedict Cumberbatch",
+			"Tom Holland",
+			"Chadwick Boseman",
+			"Jeremy Renner",
+			"Don Cheadle",
+			"Chris Pratt",
+			"Josh Brolin",
+			"Aaron Taylor-Johnson",
+			"Samuel L. Jackson",
+    ]
+];
+$catalog[204] = [
+    "title" => "Avengers: Endgame",
+		"img" => "img/media/endgame.jpg",
+	  "genre" => "Fantasy/Sci-fi",
+	  "format" => "DVD/Streaming",
+	  "year" => 2019,
+	  "category" => "Movies",
+	  "director" => "Anthony Russo",
+									"Joe Russo",
+	  "writers" => [
+			"Christopher Markus",
+			"Stephen McFeely"
+
+	  ],
+	  "stars" => [
+			"Robert Downey Jr.",
+			"Chris Evans",
+			"Chris Hemsworth",
+			"Mark Ruffalo",
+			"Scarlett Johansson",
+			"Benedict Cumberbatch",
+			"Tom Holland",
+			"Chadwick Boseman",
+			"Jeremy Renner",
+			"Don Cheadle",
+			"Paul Rudd",
+			"Brie Larson",
+			"Chris Pratt",
+			"Josh Brolin",
+			"Samuel L. Jackson",
+	  ]
+	];
+//Music
+$catalog[301] = [
+	"title" => "The Warm Up",
+	"img" => "img/media/warm up.jpg",
+	"genre" => "Hiphop, Rap",
+	"format" => "Streaming",
+	"year" => 2009,
+	"category" => "Music",
+	"artist" => "J. Cole"
+];
+
+$catalog[302] = [
+	"title" => "2014 Forest Hills Drive",
+	"img" => "img/media/fhd.jpg",
+	"genre" => "Hiphop, Rap",
+	"format" => "CD, Streaming Services",
+	"year" => 2014,
+	"category" => "Music",
+	"artist" => "J. Cole"
+];
+$catalog[303] = [
+	"title" => "4 Your Eyez Only",
+	"img" => "img/media/4ureyes.jpg",
+	"genre" => "Hiphop, Rap",
+	"format" => "CD, Streaming Services",
+	"year" => 2016,
+	"category" => "Music",
+	"artist" => "J. Cole"
+];
+$catalog[304] = [
+	"title" => "KOD",
+	"img" => "img/media/kod.jpg",
+	"genre" => "Hiphop, Rap",
+	"format" => "CD, Streaming Services",
+	"year" => 2018,
+	"category" => "Music",
+	"artist" => "J. Cole"
+];
+?>
